70cf700cb13fd393a9f9dcfc3d06b4fdcbbb5375
jo030225 <whwngur123@naver.com>
2022-09-21 01:10:31 +0900

    [CREATE] project init

---
+++ TestCodeExample/TestCodeExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCodeExample
+//
+//  Created by 조주혁 on 2022/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
